8ce9f6956ac483db8ac1cb9410e8de75f78b45d0
scoefield <scoefield@163.com>
2020-10-19 23:40:12 +0800

    添加waitgroup示例demo

---
+++ gowaitgroup/waitgroupDemo.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(2) //设置计数器，数值即为goroutine的个数
+	go func() {
+		//Do some work
+		time.Sleep(1*time.Second)
+
+		fmt.Println("Goroutine 1 finished!")
+		wg.Done() //goroutine执行结束后将计数器减1
+	}()
+
+	go func() {
+		//Do some work
+		time.Sleep(2*time.Second)
+
+		fmt.Println("Goroutine 2 finished!")
+		wg.Done() //goroutine执行结束后将计数器减1
+	}()
+
+	wg.Wait() //主goroutine阻塞等待计数器变为0
+	fmt.Printf("All Goroutine finished!")
+}
