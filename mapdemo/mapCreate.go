516e771ba9fe2a040cc663342e53db99ec7d852f
scoefield <scoefield@163.com>
2020-10-20 21:47:33 +0800

    添加map的demo

---
+++ mapdemo/mapCreate.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var countryCapitalMap map[string]string
+	/* 创建集合 */
+	countryCapitalMap = make(map[string]string)
+
+	/* map 插入 key-value 对，各个国家对应的首都 */
+	countryCapitalMap["France"] = "Paris"
+	countryCapitalMap["Italy"] = "Rome"
+	countryCapitalMap["Japan"] = "Tokyo"
+	countryCapitalMap["India"] = "New Delhi"
+
+	/* 使用 key 输出 map 值 */
+	for country := range countryCapitalMap {
+		fmt.Println("Capital of",country,"is",countryCapitalMap[country])
+	}
+
+	/* 查看元素在集合中是否存在 */
+	captial, ok := countryCapitalMap["United States"]
+	/* 如果 ok 是 true, 则存在，否则不存在 */
+	if ok {
+		fmt.Println("Capital of United States is", captial)
+	}else {
+		fmt.Println("Capital of United States is not present")
+	}
+}
