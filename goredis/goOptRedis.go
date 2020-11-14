e7aefc86b03fa82a1e9cf80710a0c2454640c1ae
scoefield <scoefield@163.com>
2020-11-14 22:08:41 +0800

    go操作redis数据库示例

---
+++ goredis/goOptRedis.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis"
+	"time"
+
+	"github.com/go-redis/redis/v8" // 注意导入的是新版本
+)
+
+var (
+	rdb *redis.Client
+)
+
+// 初始化连接
+func initClient() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:16379",
+		Password: "",  // no password set
+		DB:       0,   // use default DB
+		PoolSize: 100, // 连接池大小
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = rdb.Ping(ctx).Result()
+	return err
+}
+
+// 连接哨兵模式
+//func initClient()(err error){
+//	rdb := redis.NewFailoverClient(&redis.FailoverOptions{
+//		MasterName:    "master",
+//		SentinelAddrs: []string{"x.x.x.x:26379", "xx.xx.xx.xx:26379", "xxx.xxx.xxx.xxx:26379"},
+//	})
+//	_, err = rdb.Ping().Result()
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
+
+// 连接redis集群模式
+//func initClient()(err error){
+//	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+//		Addrs: []string{":7000", ":7001", ":7002", ":7003", ":7004", ":7005"},
+//	})
+//	_, err = rdb.Ping().Result()
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
+
+func V8Example() {
+	ctx := context.Background()
+	if err := initClient(); err != nil {
+		return
+	}
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+}
+
+// set/get示例
+func redisExample() {
+	err := rdb.Set("score", 100, 0).Err()
+	if err != nil {
+		fmt.Printf("set score failed, err:%v\n", err)
+		return
+	}
+
+	val, err := rdb.Get("score").Result()
+	if err != nil {
+		fmt.Printf("get score failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("score", val)
+
+	val2, err := rdb.Get("name").Result()
+	if err == redis.Nil {
+		fmt.Println("name does not exist")
+	} else if err != nil {
+		fmt.Printf("get name failed, err:%v\n", err)
+		return
+	} else {
+		fmt.Println("name", val2)
+	}
+}
+
+// zset示例
+func redisExample2() {
+	zsetKey := "language_rank"
+	languages := []redis.Z{
+		redis.Z{Score: 90.0, Member: "Golang"},
+		redis.Z{Score: 98.0, Member: "Java"},
+		redis.Z{Score: 95.0, Member: "Python"},
+		redis.Z{Score: 97.0, Member: "JavaScript"},
+		redis.Z{Score: 99.0, Member: "C/C++"},
+	}
+	// ZADD
+	num, err := rdb.ZAdd(zsetKey, languages...).Result()
+	if err != nil {
+		fmt.Printf("zadd failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("zadd %d succ.\n", num)
+
+	// 把Golang的分数加10
+	newScore, err := rdb.ZIncrBy(zsetKey, 10.0, "Golang").Result()
+	if err != nil {
+		fmt.Printf("zincrby failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("Golang's score is %f now.\n", newScore)
+
+	// 取分数最高的3个
+	ret, err := rdb.ZRevRangeWithScores(zsetKey, 0, 2).Result()
+	if err != nil {
+		fmt.Printf("zrevrange failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
+
+	// 取95~100分的
+	op := redis.ZRangeBy{
+		Min: "95",
+		Max: "100",
+	}
+	ret, err = rdb.ZRangeByScoreWithScores(zsetKey, op).Result()
+	if err != nil {
+		fmt.Printf("zrangebyscore failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
+}
+
+// 按通配符删除key
+func exampleTongPeiFu() {
+	ctx := context.Background()
+	iter := rdb.Scan(ctx, 0, "prefix*", 0).Iterator()
+	for iter.Next(ctx) {
+		err := rdb.Del(ctx, iter.Val()).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+	if err := iter.Err(); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+}
